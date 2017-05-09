41053158bda1735a6f1b76ecfd79325b437b9e61
George Higbie <gehigbie@gmail.com>
2017-05-09 21:18:24 +0000

    Add server start

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const express = require("express");
+const express = require("express"),
+          app = express();
+
+const port = process.env.PORT,
+        ip = process.env.IP;
+
+app.listen(port, ip, () => {
+   console.log(`Server is up on port ${port}`);
+});
