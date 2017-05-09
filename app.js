a73fc744381530e1279423e0568b436a61173246
George Higbie <gehigbie@gmail.com>
2017-05-09 21:22:18 +0000

    Add notfound view

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express"),
 
 const port = process.env.PORT,
         ip = process.env.IP;
+        
+app.use("view engine", "ejs");
 
 app.listen(port, ip, () => {
    console.log(`Server is up on port ${port}`);
